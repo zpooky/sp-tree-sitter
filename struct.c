a40a37185e52b59d4f7fcd8ab8fcb38f79962c6d
Fredrik M Olsson <fredriol@axis.com>
2021-10-29 15:41:59 +0200

    WIP
    Change-Id: Idfce427f522d467943ce50e17cee6181252656e1

--- struct.c
+++ struct.c
@@ -850,6 +850,24 @@ __format(struct sp_ts_Context *ctx,
       result->complex_raw    = strdup(sp_str_c_str(&buf_tmp));
       result->complex_printf = true;
       sp_str_free(&buf_tmp);
+
+    } else if (strcmp(result->type, "GVariant") == 0) {
+      sp_str buf_tmp;
+      sp_str_init(&buf_tmp, 0);
+      result->format = "%s";
+      if (result->pointer) {
+        sp_str_appends(&buf_tmp, pprefix, result->variable, " ? ",
+                       "g_variant_print(", pprefix, result->variable,
+                       ", FALSE)", " : \"NULL\"", NULL);
+      } else {
+        sp_str_appends(&buf_tmp, "g_variant_print(&", pprefix, result->variable,
+                       ", FALSE)", NULL);
+      }
+      free(result->complex_raw);
+      result->complex_raw    = strdup(sp_str_c_str(&buf_tmp));
+      result->complex_printf = true;
+      sp_str_free(&buf_tmp);
+
     } else if (strcmp(result->type, "uchar") == 0 || //
                strcmp(result->type, "unsigned char") == 0 || //
                strcmp(result->type, "guchar") == 0 || //
