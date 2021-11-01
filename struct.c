c4aabae7b5e06d400867a57929f1c7b439a35d60
Fredrik M Olsson <fredriol@axis.com>
2021-11-01 12:20:48 +0100

    imp
    Change-Id: I9362710caf88432b4e5fa95fcf83ebd778084fa5

--- struct.c
+++ struct.c
@@ -905,6 +905,24 @@ __format(struct sp_ts_Context *ctx,
       result->complex_printf = true;
       sp_str_free(&buf_tmp);
 
+    } else if (strcmp(result->type, "GDBusConnection") == 0) {
+      sp_str buf_tmp;
+      sp_str_init(&buf_tmp, 0);
+      result->format = "%s";
+      /* ALTERNATILVY g_dbus_connection_get_guid() instead */
+      if (result->pointer) {
+        sp_str_appends(&buf_tmp, pprefix, result->variable,
+                       " ? g_dbus_connection_get_unique_name(", pprefix,
+                       result->variable, ")", " : \"NULL\"", NULL);
+      } else {
+        sp_str_appends(&buf_tmp, "g_dbus_connection_get_unique_name(&", pprefix,
+                       result->variable, ")", NULL);
+      }
+      free(result->complex_raw);
+      result->complex_raw    = strdup(sp_str_c_str(&buf_tmp));
+      result->complex_printf = true;
+      sp_str_free(&buf_tmp);
+
     } else if (strcmp(result->type, "uchar") == 0 || //
                strcmp(result->type, "unsigned char") == 0 || //
                strcmp(result->type, "guchar") == 0 || //
