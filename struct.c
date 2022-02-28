591513557457c57a1d74a51e85fdcdea9bca6e7c
Fredrik M Olsson <fredriol@axis.com>
2022-02-28 12:07:44 +0100

    imp
    Change-Id: I7ccd53dec8d7ca36471b1585a962f125dfe54b84

--- struct.c
+++ struct.c
@@ -1269,6 +1269,30 @@ struct _GTypeQuery {
       result->complex_raw    = strdup(sp_str_c_str(&buf_tmp));
       result->complex_printf = true;
       sp_str_free(&buf_tmp);
+    } else if (strcmp(result->type, "GObjectClass") == 0) {
+#if 0
+struct  _GObjectClass {
+  GTypeClass   g_type_class;
+  /*< private >*/
+  GSList      *construct_properties;
+  /*< public >*/
+  /* seldom overridden */
+  GObject*   (*constructor)     (GType                  type, guint                  n_construct_properties, GObjectConstructParam *construct_properties);
+  /* overridable methods */
+  void       (*set_property)		(GObject        *object, guint           property_id, const GValue   *value, GParamSpec     *pspec);
+  void       (*get_property)		(GObject        *object, guint           property_id, GValue         *value, GParamSpec     *pspec);
+  void       (*dispose)			(GObject        *object);
+  void       (*finalize)		(GObject        *object);
+  /* seldom overridden */
+  void       (*dispatch_properties_changed) (GObject      *object, guint	   n_pspecs, GParamSpec  **pspecs);
+  /* signals */
+  void	     (*notify)			(GObject	*object, GParamSpec	*pspec);
+  /* called when done constructing */
+  void	     (*constructed)		(GObject	*object);
+  /*< private >*/
+  gsize		flags;
+};
+#endif
     } else if (strcmp(result->type, "GTypeInstance") == 0 ||
                strcmp(result->type, "_GTypeInstance") == 0) {
 #if 0
